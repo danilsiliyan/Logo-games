f24cc6f8dc30c90232fddde8e501a796ec7d7bd8
danilsiliyan <152623800+danilsiliyan@users.noreply.github.com>
2023-12-07 14:05:26 +0300

    Add files via upload

---
+++ Avtomatizaciya v slogax/scripts/project/scriptsInEvents.js
@@ -0,0 +1,36 @@
+
+
+
+const scriptsInEvents = {
+
+	async SpisokSobytij_Event58_Act8(runtime, localVars)
+	{
+		YaGames
+		    .init()
+		    .then(ysdk => {
+		     ysdk.adv.showFullscreenAdv();
+		});
+	},
+
+	async SpisokSobytij_Event73_Act8(runtime, localVars)
+	{
+		YaGames
+		    .init()
+		    .then(ysdk => {
+		     ysdk.adv.showFullscreenAdv();
+		});
+	},
+
+	async SpisokSobytij_Event134_Act4(runtime, localVars)
+	{
+		YaGames
+		    .init()
+		    .then(ysdk => {
+		     ysdk.adv.showFullscreenAdv();
+		});
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
