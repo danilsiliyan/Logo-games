33e968b40a4248d2fa28cabfce4fd76967f19eed
danilsiliyan <152623800+danilsiliyan@users.noreply.github.com>
2024-07-07 19:09:27 +0300

    Add files via upload

---
+++ диагностика/scripts/project/scriptsInEvents.js
@@ -0,0 +1,18 @@
+
+
+
+const scriptsInEvents = {
+
+	async SpisokSobiti_Event136_Act9(runtime, localVars)
+	{
+		YaGames
+		    .init()
+		    .then(ysdk => {
+		     ysdk.adv.showFullscreenAdv();
+		});
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
